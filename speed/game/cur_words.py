69a9f7afe8b0066b391226d4002a218d1affe1a2
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-10-30 22:02:48 -0600

    Convert from set to list

--- speed/game/cur_words.py
+++ speed/game/cur_words.py
@@ -13,7 +13,7 @@ class CurWords:
             except FileNotFoundError:
                 pass
         self._max_words = constants.STARTING_WORDS
-        self._word_store = set()
+        self._word_store = []
 
     @property
     def max_words(self):
@@ -35,7 +35,7 @@ class CurWords:
         while word_fetch == '' or self.check_word_match(word_fetch):
             word_fetch = rand_list.new_word
         built_word = Word(word_fetch, x, y, max_vel=vel)
-        self._word_store.add(built_word)
+        self._word_store.append(built_word)
 
     def check_word_match(self, compare_str):
         for w in self._word_store:
@@ -44,7 +44,9 @@ class CurWords:
         return False
     
     def remove_word(self, to_remove):
-        self._word_store.remove(to_remove)
+        for w_index, w in enumerate(self._word_store):
+            if w.word == to_remove:
+                self._word_store.pop(w_index)
     
     def move_words(self):
         for word in self._word_store:
